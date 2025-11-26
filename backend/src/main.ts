bdaae7febbe2a53b782def46e492061308bbd3b4
Yisaaaa <ryansanisit19@gmail.com>
2025-11-26 20:13:09 +0800

    Add validation pipe for incoming data

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors) => {
+        const errs = errors.map((err) => ({
+          property: err.property,
+          constraints: err.constraints,
+        }));
+
+        return new BadRequestException({
+          message: 'Validation error',
+          errors: errs,
+        });
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
