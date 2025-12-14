d7738a03c4466d9b0beaf6712ca884a31de0f3da
Yisaaaa <ryansanisit19@gmail.com>
2025-12-14 15:37:32 +0800

    feat(api): add user seeder script

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in .env');
+}
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+async function main() {
+  console.log('Seeding database...');
+  console.log('Clearing existing rows');
+  await prisma.user.deleteMany();
+  await prisma.runbook.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.executionLog.deleteMany();
+
+  const hashedPass = await bcrypt.hash('hardpass', 10);
+  const user = await prisma.user.create({
+    data: {
+      email: 'marleybob@gmail.com',
+      name: 'Bob Marley',
+      passwordHash: hashedPass,
+    },
+  });
+
+  console.log('Created user: ', user);
+  console.log('Seeding completed');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
