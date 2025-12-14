366fbe033c8e267682f0911de91ba3fe6dcff2dc
Yisaaaa <ryansanisit19@gmail.com>
2025-12-14 15:39:21 +0800

    feat(api): add health check if server is running

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  RequestMethod,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
