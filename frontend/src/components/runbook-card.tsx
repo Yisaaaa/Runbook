dabd4ed24dccf50de1598d68a6c941a69fb13f91
Yisaaaa <ryansanisit19@gmail.com>
2025-12-20 20:04:20 +0800

    feat(ui): add runbook card component

--- frontend/src/components/runbook-card.tsx
+++ frontend/src/components/runbook-card.tsx
@@ -1,5 +1,7 @@
-import { BookOpen } from "lucide-react";
+import { BookOpen, Clock, Link } from "lucide-react";
 import { Button } from "./ui/button";
+import { formatDistanceToNow } from "date-fns";
+import { useAuthStore } from "@/store/authStore";
 
 interface RunbookCardProps {
   classname?: string;
@@ -14,13 +16,36 @@ export default function RunbookCard({
   updatedAt,
   owner,
 }: RunbookCardProps) {
+  const username = useAuthStore((state) => state.user?.username);
+
   return (
-    <Button variant="ghost">
-      <div className="gap-3">
-        <BookOpen className="w-5 h-5" />
-        <span className="text truncate">{title}</span>
+    <Button
+      asChild
+      variant="ghost"
+      className="group flex gap-5 p-5 py-10 w-full justify-start"
+    >
+      <div>
+        <div className="p-4 bg-muted rounded group-hover:bg-accent transition-colors">
+          <BookOpen className="w-5 h-5" />
+        </div>
+        <div className="flex flex-col items-start gap-1">
+          <p className="truncate text-base font-medium">{title}</p>
+          <div className="flex gap-4 text-muted-foreground text-sm">
+            <div className="flex gap-2 items-center">
+              <Clock />
+              <span>
+                {formatDistanceToNow(new Date(updatedAt), {
+                  addSuffix: true,
+                })}
+              </span>
+            </div>
+            <span>•</span>
+            <div className="truncate">
+              Owner: {owner === username ? "You" : username}
+            </div>
+          </div>
+        </div>
       </div>
-      <div>...</div>
     </Button>
   );
 }
