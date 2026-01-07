6106f03a8d7899f65bbe4dfbf414f306a1a79fc9
Yisaaaa <ryansanisit19@gmail.com>
2026-01-07 13:18:37 +0800

    feat(ui): add runbook type

---
+++ frontend/src/types/runbook.ts
@@ -0,0 +1,10 @@
+export interface Runbook {
+  id: number;
+  title: string;
+  runtime: string;
+  content: string;
+  privacy: string;
+  createdAt: Date;
+  userId: number;
+  createdBy: string;
+}
