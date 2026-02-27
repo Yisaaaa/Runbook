e9a9d3a5cc254b4b567c41aa06c8f7bf4d21c570
Yisaaaa <ryansanisit19@gmail.com>
2026-02-27 09:01:37 +0800

    feat(api): add request handler for stopping container

--- backend/src/containers/containers.controller.ts
+++ backend/src/containers/containers.controller.ts
@@ -12,6 +12,12 @@ export class ContainersController {
     return this.containersService.createContainer(data.runtime);
   }
 
+  @Delete('stop/:id')
+  async stopContainer(@Param('id') id: string) {
+    console.log('Stopping container with ID: ', id);
+    return this.containersService.stop(id);
+  }
+
   @Delete('remove/:id')
   async removeContainer(@Param('id') id: string) {
     console.log('Removing container with ID: ', id);
