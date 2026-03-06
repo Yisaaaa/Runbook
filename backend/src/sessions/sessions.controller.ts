8a24957551869913d37cf5a2331ebba8db1b0eed
Yisaaaa <ryansanisit19@gmail.com>
2026-03-07 07:24:34 +0800

    feat(api): add controller for sessions service

---
+++ backend/src/sessions/sessions.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { SessionsService } from './sessions.service';
+import { CreateSessionDto } from './dto/create-session.dto';
+
+@Controller('sessions')
+export class SessionsController {
+  // This controller will handle API endpoints related to session management, such as creating a new session, fetching active sessions, and ending sessions.
+
+  constructor(private readonly sessionsService: SessionsService) {}
+
+  @Post('create')
+  async createSession(@Body() data: CreateSessionDto) {
+    return this.sessionsService.createSession(data);
+  }
+
+  @Delete('end/:id')
+  async endSession(@Param('id') id: number) {
+    return this.sessionsService.endSession(id);
+  }
+
+  @Get('active/:userId/:runbookId')
+  async getActiveSessions(
+    @Param('userId') userId: number,
+    @Param('runbookId') runbookId: number,
+  ) {
+    return this.sessionsService.getActiveSessions(userId, runbookId);
+  }
+}
